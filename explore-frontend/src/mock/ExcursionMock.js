ca0b8eb8c4a70731c98835102dab37ea600d11a6
Philip Hoang <philip.t.hoang@outlook.com>
2022-08-25 18:20:25 +0200

    Big upgrades to the page

---
+++ explore-frontend/src/mock/ExcursionMock.js
@@ -0,0 +1,17 @@
+const ExcursionMock = {
+    title: "RIB-trip",
+    date: "Monday 25th of August ・ 10:00  ・ 2 hours",
+    image: "../images/image-1.jpg",
+    seats: "27 out of 30 spots available",
+    description: "Catchy text about the excursion",
+    keyInformation: [
+        "Meetup in reception at 09:50",
+        "All necessary equipment included",
+        "Lunch at 12:00",
+        "Level of difficulty: Easy"
+    ],
+    price: "40 dollars per adult, 20 dollars per child"
+}
+
+
+export default ExcursionMock
