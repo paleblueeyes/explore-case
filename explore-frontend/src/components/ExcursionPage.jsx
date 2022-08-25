148eb8dd8a9556d69702b6c0fc304c30da43fb4d
Philip Hoang <philip.t.hoang@outlook.com>
2022-08-25 22:02:08 +0200

    Added modal for booking + styling

--- explore-frontend/src/components/ExcursionPage.jsx
+++ explore-frontend/src/components/ExcursionPage.jsx
@@ -1,15 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import { Link, useParams } from "react-router-dom";
 import { db } from "../firebase.js";
 import { query, where, getDoc, doc } from "firebase/firestore";
 
 import ExcursionMock from "../mock/ExcursionMock";
+import BookingModal from "./BookingModal";
 
 const ExcursionPage = () => {
-  const [trip, setTrip] = useState({});
-  const [buttonText, setButtonText] = useState("Book activity");
-  let params = useParams();
-
   useEffect(() => {
     const getTrip = async () => {
       console.log(params);
@@ -27,20 +25,59 @@ const ExcursionPage = () => {
     };
     getTrip();
   }, []);
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  const [adultTickets, setAdultTickets] = useState(0);
+  const [childrenTickets, setChildrenTickets] = useState(0);
+  const [totalSum, setTotalSum] = useState(0);
+
+  function incrementAdultTickets() {
+    setAdultTickets(adultTickets + 1);
+    setTotalSum(totalSum + trip.cost_adult);
+  }
+
+  function decrementAdultTickets() {
+    setAdultTickets(adultTickets - 1);
+    setTotalSum(totalSum - trip.cost_adult);
+  }
+
+  function incrementChildrenTickets() {
+    setChildrenTickets(childrenTickets + 1);
+    setTotalSum(totalSum + trip.cost_child);
+  }
+
+  function decrementChildrenTickets() {
+    setChildrenTickets(childrenTickets - 1);
+    setTotalSum(totalSum - trip.cost_child);
+  }
+
+  function calculateTotalSum() {}
+
+  const [trip, setTrip] = useState({});
+  const [buttonText, setButtonText] = useState("Book activity");
+  let params = useParams();
 
   const handleClick = () => {
     setButtonText("Booked");
   };
 
   return (
-    <div className="bg-deep-blue min-h-screen text-white shadow overflow-hidden sm:rounded-lg">
+    <div className="bg-deep-blue min-h-screen text-white shadow overflow-hidden sm:rounded-lg px-5">
       <div className="flex">
         <Link to="/excursions">
           <img className="p-2 h-16" src="/images/Frame.png"></img>
         </Link>
       </div>
       <div className="sm:px-6 rounded-sm">
-        <img src={ExcursionMock.image} />
+        <img src={trip.url} />
       </div>
 
       <div className="pl-2 pt-2">
@@ -66,20 +103,127 @@ const ExcursionPage = () => {
         </div>
         <div>
           <h2 className="text-xl pt-4">Price</h2>
-          <p className="text-sm">Adults: {trip.cost_adult} NOK</p>
-          <p className="text-sm">Children: {trip.cost_child} NOK</p>
+          <p className="text-sm">Adults: {trip.cost_adult} USD</p>
+          <p className="text-sm">Children: {trip.cost_child} USD</p>
           <p className="text-sm">
-            Cancellation fee: {trip.cancellation_fee} NOK
+            Cancellation fee: {trip.cancellation_fee} USD
           </p>
         </div>
 
         <div className="pt-4">
           <button
+            type="button"
             class="border w-36 rounded-3xl h-12 px-6 text-deep-blue transition-colors duration-150 bg-white rounded-lg focus:shadow-outline hover:bg-white mr-4"
-            onClick={handleClick}
+            onClick={openModal}
           >
-            {buttonText}
+            Book
           </button>
+
+          <Transition appear show={isOpen} as={Fragment}>
+            <Dialog
+              as="div"
+              className="fixed inset-0 z-10 overflow-y-auto"
+              onClose={closeModal}
+            >
+              <div className="min-h-screen px-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Dialog.Overlay className="fixed inset-0" />
+                </Transition.Child>
+
+                {/* This element is to trick the browser into centering the modal contents. */}
+                <span
+                  className="inline-block h-screen align-middle"
+                  aria-hidden="true"
+                >
+                  &#8203;
+                </span>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      Confirm Booking
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <div className="flex justify-between items-center border-t">
+                        <p className="text-sm text-gray-500 pt-2">Adult(s)</p>
+                        <div className="flex justify-between w-16 ml-4 items-center pt-3">
+                          <img
+                            className="h-4"
+                            onClick={decrementAdultTickets}
+                            src="/images/decrement.png"
+                          />
+                          <p>{adultTickets}</p>
+
+                          <img
+                            className="h-4"
+                            onClick={incrementAdultTickets}
+                            src="/images/increment.png"
+                          />
+                        </div>
+                      </div>
+
+                      <div className="flex justify-between items-center border-t">
+                        <p className="text-sm text-gray-500  pt-2">Children</p>
+                        <div className="flex justify-between w-16 ml-4 items-center pt-3">
+                          <img
+                            className="h-4"
+                            onClick={decrementChildrenTickets}
+                            src="/images/decrement.png"
+                          />
+                          <p>{childrenTickets}</p>
+
+                          <img
+                            className="h-4"
+                            onClick={incrementChildrenTickets}
+                            src="/images/increment.png"
+                          />
+                        </div>
+                      </div>
+
+                      <div className="flex justify-between border-t">
+                        <p className="text-sm text-gray-500 pt-2">Price</p>
+                        <p className="mr-3">{totalSum}</p>
+                      </div>
+                      <div>
+                        <p>
+                          The cost will be added to your Explore Arctic payment
+                          card. <b>Cancellation fee: 10$ </b>
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="mt-4">
+                      <button
+                        type="button"
+                        className="inline-flex justify-center px-4 py-2 text-sm text-red-900 bg-red-100 border border-transparent rounded-md hover:bg-red-200 duration-300"
+                        onClick={closeModal}
+                      >
+                        Book
+                      </button>
+                    </div>
+                  </div>
+                </Transition.Child>
+              </div>
+            </Dialog>
+          </Transition>
         </div>
       </div>
     </div>
