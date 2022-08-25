c21d9e006fdc1001de6cb5404747f8362b7bbc7e
Andreas Rolfsvåg <andreas.rolfsvag@NO-DFH4745R9Q.local>
2022-08-25 14:40:46 +0200

    Link excursion list with individual excursion

--- explore-frontend/src/index.js
+++ explore-frontend/src/index.js
@@ -1,31 +1,20 @@
-import React from 'react';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import ExcursionList from './components/ExcursionList';
-import ExcursionPage from './components/ExcursionPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ExcursionList from "./components/ExcursionList";
+import ExcursionPage from "./components/ExcursionPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-
-        </Route>
-        <Route path="excursions" element={<ExcursionList />}>
-          
-        </Route>
-        <Route path="page" element={<ExcursionPage />}>
-
-        </Route>
-
+        <Route path="/" element={<App />} />
+        <Route path="excursions/:excursionId" element={<ExcursionPage />} />
+        <Route path="excursions" element={<ExcursionList />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
