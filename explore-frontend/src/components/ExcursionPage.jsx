39796a5bc72197cab1124eda0d501d03048971fa
Philip Hoang <philip.t.hoang@outlook.com>
2022-08-25 14:52:47 +0200

    Git merge sovled

--- explore-frontend/src/components/ExcursionPage.jsx
+++ explore-frontend/src/components/ExcursionPage.jsx
@@ -2,114 +2,70 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ExcursionPage = () => {
-<<<<<<< HEAD
     const [register, setRegister] = useState(false);
-    const [buttonText, setButtonText] = useState("Book")
+    let params = useParams();
+    console.log(params);
 
-    function handleClick() {
-        setButtonText("Booked");
-    }
-=======
-  const [register, setRegister] = useState(false);
-  let params = useParams();
-  console.log(params);
->>>>>>> master
+    return (
+        <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+            <div className="px-4 py-5 sm:px-6 rounded-sm">
+                <img src="images/basecamp.jpeg" />
+            </div>
 
-  return (
-    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-      <div className="px-4 py-5 sm:px-6 rounded-sm">
-        <img src="images/basecamp.jpeg" />
-      </div>
-
-<<<<<<< HEAD
             <div className="borter-t border-gray-200">
                 <dl>
-                    <div className="bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt className="text-sm font-medium text-gray-500">Excursion name</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Title</dd>
+                    <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">
+                            Excursion name
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            Title
+                        </dd>
                     </div>
                     <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt className="text-sm font-medium text-gray-500">Information</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Information yet to come</dd>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            Information yet to come
+                        </dd>
                     </div>
-                    <div className="bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt className="text-sm font-medium text-gray-500">Start date</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">25. August 2022</dd>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            25. August 2022
+                        </dd>
                     </div>
                     <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt className="text-sm font-medium text-gray-500">Available seats</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">27</dd>
-                    </div>
-                    <div className="bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt className="text-sm font-medium text-gray-500">Price for adults</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">500 NOK</dd>
+                        <dt className="text-sm font-medium text-gray-500">
+                            Available seats
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            27
+                        </dd>
                     </div>
                     <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt className="text-sm font-medium text-gray-500">Price for children</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">250 NOK</dd>
+                        <dt className="text-sm font-medium text-gray-500">
+                            Price for adults
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            500 NOK
+                        </dd>
+                    </div>
+                    <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">
+                            Price for children
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            250 NOK
+                        </dd>
                     </div>
 
-                    <button class="fixed bottom-0 w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 mr-4"
-                        onClick={handleClick}
-                    >
-                        {buttonText}
+                    <button class="fixed w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 mr-4">
+                        Register
                     </button>
-=======
-      <div className="borter-t border-gray-200">
-        <dl>
-          <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
-              Excursion name
-            </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              Title
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Information</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              Information yet to come
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Start date</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              25. August 2022
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
-              Available seats
-            </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              27
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
-              Price for adults
-            </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              500 NOK
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
-              Price for children
-            </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              250 NOK
-            </dd>
-          </div>
-
-          <button class="fixed w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 mr-4">
-            Register
-          </button>
-        </dl>
-      </div>
-    </div>
-  );
+                </dl>
+            </div>
+        </div>
+    );
 };
->>>>>>> master
 
 export default ExcursionPage;
